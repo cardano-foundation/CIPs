de9fd3b8ebd28b24a68f3ba8918fff9f6e427f72
Nicolas Henin <nicolas.henin@iohk.io>
2025-03-27 19:02:35 +0200

    extracted detailed analysis into CPD + updated CPS

--- CPS-00??/CPD/graph/forking_probabilities.py
+++ CPS-00??/CPD/graph/forking_probabilities.py
@@ -2,12 +2,13 @@
 
 import sys
 import math
-import time
 import scipy
 from decimal import Decimal
 from joblib import Parallel, delayed
 import scipy.special
 import argparse
+from tabulate import tabulate
+
 
 # Considered adversaral stakes in percentage
 stakes = [0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.25, 0.3, 0.33, 0.4, 0.45, 0.49] 
@@ -58,14 +59,50 @@ def eg(s, precision=10, cores=2):
 
 # Computes and print the expectation of grinding attempts for all adversaries
 def all_egs(precision=10, cores=1):
- start = time.time()
  results = Parallel(n_jobs=cores)(delayed(eg)(s, precision) for s in stakes)
  res = sorted(results, key=lambda x : x[0])
- end = time.time()
- print("\n--- precision={0} computed in {1:.2f}s".format(precision, end-start))
- print("stake \t E(#grinding attempts)")
- for r in res:
-  print("{0:.3f}:\t {1:.3E}".format(r[0], r[1]))
+ # Printing tables (use tabulate's option tablefmt="plain" to have no lines)
+ headers = ["stake (%)"] + ["{:.1f}%".format(s*100) for s in stakes]
+ table = [["E(g)"] + ["{0:.2E}".format(r[1]) for r in res]]
+ print("\nTable of grinding attempts")
+ print(tabulate(table, headers, tablefmt='orgtbl'))
+
+
+def proba_diff(diff, s, precision=10):
+ ps = []
+ for i in range(precision+1):
+  acc = Decimal(1)
+  for j in range(1,i+1):
+   acc = acc * Decimal((diff+2*i+1-j)/j) * Decimal(s) * Decimal(1-s)
+  for j in range(i+1,diff+i+1):
+   acc = acc * Decimal((diff+2*i+1-j)/j) * Decimal(s)
+  for (j, di) in enumerate(ps):
+   acc_d = di
+   for l in range(1, i-j+1):
+    acc_d = Decimal(acc_d) * Decimal((2*i-2*j+1-l)/l) * Decimal(s) * Decimal(1-s)
+   acc = acc - acc_d
+  ps.append(acc)
+ return (s, sum(ps))
+
+def tables(precision=10, cores=1):
+ # Computing the tables of probabilities
+ table = []
+ pows = [1,2,4,8,16,32,64,128,256]
+ for p in pows:
+  results = Parallel(n_jobs=cores)(delayed(proba_diff)(p, s, precision) for s in stakes)
+  row = sorted(results, key=lambda x: x[0])
+  table.append([r[1] for r in row])
+ # Printing tables (use tabulate's option tablefmt="plain" to have no lines)
+ headers = ["|Xa - Xh| vs stake"] + ["{:.1f}%".format(s*100) for s in stakes]
+ # Printing the tables of probabilities
+ table_proba = [[pows[i]] + ["{:.3E}".format(el) for el in row] for (i,row) in enumerate(table)]
+ print("\nTable of probabilities")
+ print(tabulate(table_proba, headers, tablefmt='orgtbl'))
+ # Printing the tables of frequencies (per year)
+ table_year = [[pows[i]] + ["{:.2E}".format(5./(float(el)*365.0)) if float(el) !=0 else "-" for el in row] for (i,row) in enumerate(table)]
+ print("\nTable of frequencies (year)")
+ print(tabulate(table_year, headers, tablefmt='orgtbl'))
+
 
 def parseArguments():
     # Create argument parser
@@ -86,6 +123,12 @@ def parseArguments():
 if __name__ == '__main__':
   # Parse arguments
   args = parseArguments()
+  precision = args.precision
+  cores = min(args.cores, nb_stakes)
+  print("Printing forking's figures with precision={}".format(precision))
   
   # Run function
-  all_egs(precision=args.precision, cores=min(args.cores, nb_stakes))
+  all_egs(precision, cores)
+  
+  # Run tables
+  tables(precision, cores)
